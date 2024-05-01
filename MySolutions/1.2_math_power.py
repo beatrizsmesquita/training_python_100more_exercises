b3c272551f2e64750ccb13e9c553c83f7c612824
Beatriz Sofia Mesquita Gonçalves <beatriz-s-goncalves@telecom.pt>
2024-05-01 22:49:10 +0100

    Added solution for 1.22
    math power

---
+++ MySolutions/1.2_math_power.py
@@ -0,0 +1,11 @@
+# Take two numbers from the users. Calculate the result of second number power of the first number.
+
+var1 = float(input("Insert your first number: "))
+var2 = float(input("Insert your second number: "))
+
+#print(f"{var2} to the power of {var1} is equal to: ", var2**var1)
+
+
+## or using pow
+
+print(f"{var2} to the power of {var1} is equal to: ", pow(var2,var1))
